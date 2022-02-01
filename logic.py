d77cf336538dabaac87e157952b7656029e2acfb
Marcin Galazyn <m.galazynn@gmail.com>
2022-02-01 14:07:02 +0100

    logic_basic

---
+++ logic.py
@@ -0,0 +1,12 @@
+import sys 
+import requests, lxml, bs4 
+
+# Get the price from API
+
+# Check the Fear and Greed index at alternative.me 
+
+# Check if it's above/under stop loss
+
+# If it's IT will message you, if you will not anserw on your own after 20 min. It's going to take action. 
+
+# Action based on your own preferences which you configurate (stop loss, action)
